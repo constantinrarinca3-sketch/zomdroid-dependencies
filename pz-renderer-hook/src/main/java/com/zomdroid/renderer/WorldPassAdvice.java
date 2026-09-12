894faa93b6749692d7c1348877a3608ad0bfa7ba
constantinrarinca3-sketch <constantinrarinca3@gmail.com>
2026-09-12 14:41:47 +0300

    v3: census exact WORLD state before compile

--- pz-renderer-hook/src/main/java/com/zomdroid/renderer/WorldPassAdvice.java
+++ pz-renderer-hook/src/main/java/com/zomdroid/renderer/WorldPassAdvice.java
@@ -2,18 +2,15 @@ package com.zomdroid.renderer;
 
 import net.bytebuddy.asm.Advice;
 import zombie.core.PZWorldCompiler;
+import zombie.core.WorldDrawCensus;
+import zombie.core.sprite.SpriteRenderState;
 
 public final class WorldPassAdvice {
-    private WorldPassAdvice() {
-    }
+    private WorldPassAdvice() {}
 
-    @Advice.OnMethodEnter
-    public static boolean enter() {
-        return PZWorldCompiler.enterWorldPass();
-    }
-
-    @Advice.OnMethodExit(onThrowable = Throwable.class)
-    public static void exit(@Advice.Enter boolean previous) {
-        PZWorldCompiler.leaveWorldPass(previous);
+    @Advice.OnMethodEnter(skipOn = Advice.OnNonDefaultValue.class)
+    public static boolean enter(@Advice.Argument(0) SpriteRenderState state) {
+        WorldDrawCensus.observe(state);
+        return PZWorldCompiler.compileWorldState(state);
     }
 }
